eba1f483222aa21908d6738656881afa9874e1ab
Agustin Costa <agustincostaraus@gmail.com>
2024-12-02 18:44:42 -0300

    Add noise option to original scripts

--- scripts/run_full_data.py
+++ scripts/run_full_data.py
@@ -9,18 +9,25 @@ from types import SimpleNamespace
 ROOT = sys.argv[1]
 EXP = sys.argv[2]
 RUN = int(sys.argv[3])
+RAND_LABEL_FRAC = float(sys.argv[4])
+RAND_LABEL_SEED = int(sys.argv[5])
+EPOCHS = int(sys.argv[6])
 META_MODEL_SEED, META_TRAIN_SEED, SEED_INCR = 42, 4242, 424242
 EP_STEPS = 390
-DATA_DIR = os.path.join(ROOT, 'data')
-EXPS_DIR = os.path.join(ROOT, 'exps')
+DATA_DIR = os.path.join(ROOT, "data")
+EXPS_DIR = os.path.join(ROOT, "exps")
 
 # arguments
 args = SimpleNamespace()
 # data
 args.data_dir = DATA_DIR
-args.dataset = 'cifar10'
-args.noise_level = 0      # percentage of training samples to add noise to
-args.noise_type = "label" # "image" to add noise at the pixel level or "label" to mislabel the image
+args.dataset = "cifar10"
+args.noise_level = 0  # percentage of training samples to add noise to
+args.noise_type = (
+    "label"  # "image" to add noise at the pixel level or "label" to mislabel the image
+)
+args.random_label_fraction = RAND_LABEL_FRAC
+args.random_label_seed = RAND_LABEL_SEED
 # subsets
 args.subset = None
 args.subset_size = None
@@ -28,7 +35,7 @@ args.scores_path = None
 args.subset_offset = None
 args.random_subset_seed = None
 # model
-args.model = 'resnet18_lowres'
+args.model = "resnet18_lowres"
 args.model_seed = META_MODEL_SEED + RUN * SEED_INCR
 args.load_dir = None
 args.ckpt = 0
@@ -41,14 +48,14 @@ args.lr_vitaly = False
 args.decay_factor = 0.2
 args.decay_steps = [60 * EP_STEPS, 120 * EP_STEPS, 160 * EP_STEPS]
 # training
-args.num_steps = 200 * EP_STEPS
+args.num_steps = EPOCHS * EP_STEPS
 args.train_seed = META_TRAIN_SEED + RUN * SEED_INCR
-args.train_batch_size = 128
+args.train_batch_size = 1024
 args.test_batch_size = 1024
 args.augment = True
 args.track_forgetting = True
 # checkpoints
-args.save_dir = os.path.abspath(os.path.join(EXPS_DIR, f'{EXP}/run_{RUN}'))
+args.save_dir = os.path.abspath(os.path.join(EXPS_DIR, f"{EXP}/run_{RUN}"))
 args.log_steps = EP_STEPS
 args.early_step = 0
 args.early_save_steps = None
